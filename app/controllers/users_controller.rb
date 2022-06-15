ed276eacb813f60a3e34ee46fee0e80985a69119
emmageo3 <kanfanyemma22@gmail.com>
2022-06-15 16:05:11 +0000

    login and register

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,16 @@
 class UsersController < ApplicationController 
+
+    skip_before_action :authorized, only: [:new, :create]
+
     def new
         @user = User.new
     end
 
     def create
-        @user = User.new(user_params)
-        if @user.save
-            redirect_to root_path, notice: "Vous avez été inscrit avec succès"
-        else
-            render
-        end 
+        @user = User.create(params.require(:user).permit(:email,        
+        :password))
+        session[:user_id] = @user.id
+        redirect_to '/welcome'
     end
 
-    def user_params
-        params.require(:user).permit(:email, :password, :password_confirm)
-    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  
-  get "inscription", to: "users#new"
-  post "inscription", to: "users#create"
-
-  root to: "users#new"
+  resources :users, only: [:new, :create]
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  get 'welcome', to: 'sessions#welcome'
+  get 'authorized', to: 'sessions#page_requires_login'
 end
